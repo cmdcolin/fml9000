a9aa0e2401c17004f4710b101aeaf2a6fa0d8bea
Colin <colin.diesh@gmail.com>
2024-08-10 17:17:30 -0400

    Start 4.10 deprecation fixes

--- src/preferences_dialog.rs
+++ src/preferences_dialog.rs
@@ -1,22 +1,14 @@
 use crate::settings::{write_settings, FmlSettings};
-use gtk::glib;
 use adw::prelude::*;
-use gtk::{Button, Entry, FileChooserAction, FileChooserDialog, Orientation, ResponseType};
+use gtk::gio;
+use gtk::glib;
+use gtk::{Button, Entry, FileDialog, Orientation};
 use std::cell::RefCell;
 use std::rc::Rc;
 
 pub async fn dialog<W: IsA<gtk::Window>>(wnd: Rc<W>, settings: Rc<RefCell<FmlSettings>>) {
-  let preferences_dialog = gtk::Dialog::builder()
-    .transient_for(&*wnd)
-    .modal(true)
-    .default_width(800)
-    .default_height(600)
-    .title("Preferences")
-    .build();
+  let f = gtk::Box::new(Orientation::Horizontal, 0);
 
-  let folder_box = gtk::Box::new(Orientation::Horizontal, 0);
-
-  let content_area = preferences_dialog.content_area();
   let open_button = Button::builder().label("Open folder...").build();
   let s = settings.borrow().folder.clone();
   let textbox = Entry::builder()
@@ -24,23 +16,32 @@ pub async fn dialog<W: IsA<gtk::Window>>(wnd: Rc<W>, settings: Rc<RefCell<FmlSet
     .hexpand(true)
     .build();
 
-  folder_box.append(&textbox);
-  folder_box.append(&open_button);
-  content_area.append(&folder_box);
+  f.append(&textbox);
+  f.append(&open_button);
+  let preferences_dialog = gtk::Window::builder()
+    .transient_for(&*wnd)
+    .modal(true)
+    .default_width(800)
+    .default_height(600)
+    .title("Preferences")
+    .child(&f)
+    .build();
 
-  let preferences_dialog_rc = Rc::new(preferences_dialog);
-  open_button.connect_clicked(
-    glib::clone!(@weak wnd, @weak textbox, @weak settings => move |_| {
-      let file_chooser = FileChooserDialog::new(
-        Some("Open Folder"),
-        Some(&*wnd),
-        FileChooserAction::SelectFolder,
-        &[("Open", ResponseType::Ok), ("Cancel", ResponseType::Cancel)],
-      );
-      file_chooser.set_modal(true);
-      file_chooser.connect_response(move |d: &FileChooserDialog, response: ResponseType| {
-        if response == ResponseType::Ok {
-          let file = d.file().expect("Couldn't get file");
+  open_button.connect_clicked(glib::clone!(
+    #[weak]
+    wnd,
+    #[weak]
+    textbox,
+    #[weak]
+    settings,
+    move |_| {
+      let dialog = FileDialog::builder()
+        .title("Open File")
+        .accept_label("Open")
+        .build();
+
+      dialog.open(Some(&*wnd), gio::Cancellable::NONE, move |file| {
+        if let Ok(file) = file {
           let p = file.path().expect("Couldn't get file path");
           let folder = &p.to_string_lossy();
           textbox.set_text(folder);
@@ -48,11 +49,8 @@ pub async fn dialog<W: IsA<gtk::Window>>(wnd: Rc<W>, settings: Rc<RefCell<FmlSet
           s.folder = Some(folder.to_string());
           write_settings(&s).expect("Failed to write");
         }
-        d.close();
       });
-      file_chooser.show();
-    }),
-  );
-
-  preferences_dialog_rc.run_future().await;
+    }
+  ));
+  preferences_dialog.present();
 }
